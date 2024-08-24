29798118f9ad13178dfe78e922f1c2551d98e862
Kemi1505 <oluwakemikuteyi1505@gmail.com>
2024-08-24 10:17:35 -0700

    final ssh

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,12 +1,16 @@
-#!/usr/bin/env bash
-# Setting up my client configuration file
+# Setting up client configuration file
+include stdlib
 
-file { 'ect/ssh/ssh_config':
-  	ensure => present,
-content =>"
+file_line { 'Turn off password authentication':
+  ensure => present,
+  path   => '/etc/ssh/ssh_config',
+  line   => '    PasswordAuthentication no',
+  replace => true,
+}
 
-	#client configuration
-	host*
-	IdentityFile ~/.ssh/school
-	PasswordAuthentication no
+file_line { 'Delare the identity file':
+  ensure => present,
+  path   => '/etc/ssh/ssh_config',
+  line   => '     IdentityFile ~/.ssh/school',
+  replace => true,
 }
